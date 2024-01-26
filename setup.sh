3686e87f79f5749f6828c97fb96254657d742512
Phil Weber <phil@philweber.com>
2024-01-26 04:21:00 +0000

    Correct download link

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-V=1.48.4 ARCH=amd64; echo "https://download.newrelic.com/infrastructure_agent/binaries/linux/${ARCH}/newrelic-infra_linux_${V}_${ARCH}.tar.gz" | { read url; wget "${url}"{,.sum}; shasum -a 256 --check ${url##*/}.sum; }
+wget https://download.newrelic.com/infrastructure_agent/binaries/linux/amd64/newrelic-infra_linux_1.48.4_amd64.tar.gz
 tar -xf newrelic-infra_linux_1.48.4_amd64.tar.gz
 sudo mv ./newrelic-infra/etc/newrelic-infra /etc/
 sudo mv ./newrelic-infra/usr/bin/* /usr/bin/
