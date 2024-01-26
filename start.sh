0221464c4265077ce383465c633c6ef3d0c46cf2
Phil Weber <phil@philweber.com>
2024-01-26 13:17:15 -0800

    Add README.md. Run setup.sh automatically. Check for license key before starting agent

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-sudo newrelic-infra &
+#!/bin/sh
+if [ $(grep -c "YOUR_LICENSE_KEY" newrelic-infra.yml) -gt 0 ]
+then
+    echo "Replace YOUR_LICENSE_KEY in newrelic-infra.yml with the value of the INGEST-LICENSE key for your New Relic account. (Did you remember to save your changes to the file?)"
+else
+    sudo newrelic-infra &
+fi
