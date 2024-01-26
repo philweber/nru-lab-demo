65924d5b7ce470ce067af981b4c7725a9fe0e8e3
Phil Weber <phil@philweber.com>
2024-01-25 20:42:56 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ wget https://download.newrelic.com/infrastructure_agent/binaries/linux/amd64/new
 tar -xf newrelic-infra_linux_1.48.4_amd64.tar.gz
 sudo mv ./newrelic-infra/etc/newrelic-infra /etc/
 sudo mv ./newrelic-infra/usr/bin/* /usr/bin/
-sudo mv ./newrelic-infra/var/db/newrelic-infra/ /var/db/
-sudo mv ./newrelic-infra/var/db/newrelic-infra/ /var/db/
+sudo mv ./newrelic-infra/var/db/newrelic-infra /var/db/
 sudo mkdir /var/log/newrelic-infra
 sudo mkdir /var/run/newrelic-infra
+sudo mv newrelic-infra.yml /etc/
