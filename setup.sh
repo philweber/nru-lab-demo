d1a9d56644833d3f81cbdaf9b691816a93de13d9
Phil Weber <phil@philweber.com>
2024-01-25 21:35:59 -0800

    Script changes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo apt install libcap2-bin -y
-sudo NRIA_MODE="PRIVILEGED" ./before-install.sh
+sudo ./before-install.sh
 
 # Download Infrastructure agent
 wget https://download.newrelic.com/infrastructure_agent/binaries/linux/amd64/newrelic-infra_linux_1.48.4_amd64.tar.gz
@@ -10,7 +9,8 @@ tar -xf newrelic-infra_linux_1.48.4_amd64.tar.gz
 # Move files and create directories
 sudo mv ./newrelic-infra/etc/newrelic-infra /etc/
 sudo mv ./newrelic-infra/usr/bin/* /usr/bin/
-sudo mv ./newrelic-infra/var/db/newrelic-infra /var/db/
+sudo mkdir /var/db/newrelic-infra
+sudo mv ./newrelic-infra/var/db/newrelic-infra/ /var/db/newrelic-infra
 sudo mkdir /var/log/newrelic-infra
 sudo mkdir /var/run/newrelic-infra
 sudo mv newrelic-infra.yml /etc/
