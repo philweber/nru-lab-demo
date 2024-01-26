6ce002cd57e5a86160fbbb3339c197a0b7777ea8
Phil Weber <phil@philweber.com>
2024-01-25 20:13:30 -0800

    Install newrelic-infra from tar file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# Enable New Relic GPG key
-curl -fsSL https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/newrelic-infra.gpg
-# Add New Relic Infastructure repo
-echo "deb https://download.newrelic.com/infrastructure_agent/linux/apt focal main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
-# Install New Relic Infra
-sudo apt install newrelic-infra -y
+V=1.48.4 ARCH=amd64; echo "https://download.newrelic.com/infrastructure_agent/binaries/linux/${ARCH}/newrelic-infra_linux_${V}_${ARCH}.tar.gz" | { read url; wget "${url}"{,.sum}; shasum -a 256 --check ${url##*/}.sum; }
+tar -xf newrelic-infra_linux_1.48.4_amd64.tar.gz
+sudo mv ./newrelic-infra/etc/newrelic-infra /etc/
+sudo mv ./newrelic-infra/usr/bin/* /usr/bin/
+sudo mv ./newrelic-infra/var/db/newrelic-infra/ /var/db/
+sudo mv ./newrelic-infra/var/db/newrelic-infra/ /var/db/
+sudo mkdir /var/log/newrelic-infra
+sudo mkdir /var/run/newrelic-infra
