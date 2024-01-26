857a6eae74af54df73751f36fcbd8dc79c6e0b9a
Phil Weber <phil@philweber.com>
2024-01-26 13:26:28 -0800

    Improve formatting of error message

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $(grep -c "YOUR_LICENSE_KEY" newrelic-infra.yml) -gt 0 ]
 then
-    echo "Replace YOUR_LICENSE_KEY in newrelic-infra.yml with the value of the INGEST-LICENSE key for your New Relic account. (Did you remember to save your changes to the file?)"
+    echo "\nReplace YOUR_LICENSE_KEY in newrelic-infra.yml with the value of the INGEST-LICENSE key for your New Relic account.\n(Did you remember to save your changes to the file?)\n"
 else
     sudo newrelic-infra &
 fi
