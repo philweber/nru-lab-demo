9aa1c257ab3bd7b94474669c213f888098cf9f4d
Phil Weber <phil@philweber.com>
2024-01-26 02:03:03 -0800

    Add fluent-bit to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,13 +8,20 @@ wget https://download.newrelic.com/infrastructure_agent/binaries/linux/amd64/new
 tar -xf newrelic-infra_linux_1.48.4_amd64.tar.gz
 # Move files and create directories
 sudo mv ./newrelic-infra/etc/newrelic-infra /etc/
+sudo mkdir /etc/newrelic-infra/logging.d
 sudo mv ./newrelic-infra/usr/bin/* /usr/bin/
-sudo mkdir -p /var/db/newrelic-infra
+sudo mkdir -p /var/db/newrelic-infra/newrelic-integrations/logging
 sudo mv ./newrelic-infra/var/db/newrelic-infra/ /var/db/newrelic-infra
 sudo mkdir /var/log/newrelic-infra
 sudo mkdir /var/run/newrelic-infra
-sudo mv newrelic-infra.yml /etc/
+# Download and install fluent-bit
+wget https://github.com/newrelic/fluent-bit-package/releases/download/2.0.8/fluent-bit_2.0.8_ubuntu-jammy_amd64.deb
+sudo apt install ./fluent-bit_2.0.8_ubuntu-jammy_amd64.deb -y
+sudo wget -O /var/db/newrelic-infra/newrelic-integrations/logging/out_newrelic.so https://github.com/newrelic/newrelic-fluent-bit-output/releases/download/v1.19.1/out_newrelic-linux-amd64-1.19.1.so
+sudo wget -O /var/db/newrelic-infra/newrelic-integrations/logging/parsers.conf https://raw.githubusercontent.com/newrelic/fluent-bit-package/main/parsers.conf
+
 # Clean up
+rm fluent-bit_2.0.8_ubuntu-jammy_amd64.deb
 rm newrelic-infra_linux_1.48.4_amd64.tar.gz
 rm -rf newrelic-infra/
 
