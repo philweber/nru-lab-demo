3ed428d3ea2e675dad6d95b7a26e19685073083b
Phil Weber <phil@philweber.com>
2024-01-26 03:01:40 -0800

    Update Ubuntu to 22.04 and fluent-bit to 2.0.8

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo apt update
 sudo scripts/preinst.sh
 
 # Download Infrastructure agent
@@ -15,13 +16,13 @@ sudo mv ./newrelic-infra/var/db/newrelic-infra/ /var/db/newrelic-infra
 sudo mkdir /var/log/newrelic-infra
 sudo mkdir /var/run/newrelic-infra
 # Download and install fluent-bit
-wget https://github.com/newrelic/fluent-bit-package/releases/download/1.9.3/td-agent-bit_1.9.3_ubuntu-focal_amd64.deb
-sudo apt install ./td-agent-bit_1.9.3_ubuntu-focal_amd64.deb -y
+wget https://github.com/newrelic/fluent-bit-package/releases/download/2.0.8/fluent-bit_2.0.8_ubuntu-jammy_amd64.deb
+sudo apt install ./fluent-bit_2.0.8_ubuntu-jammy_amd64.deb -y
 sudo wget -O /var/db/newrelic-infra/newrelic-integrations/logging/out_newrelic.so https://github.com/newrelic/newrelic-fluent-bit-output/releases/download/v1.19.1/out_newrelic-linux-amd64-1.19.1.so
 sudo wget -O /var/db/newrelic-infra/newrelic-integrations/logging/parsers.conf https://raw.githubusercontent.com/newrelic/fluent-bit-package/main/parsers.conf
 
 # Clean up
-rm td-agent-bit_1.9.3_ubuntu-focal_amd64.deb
+rm fluent-bit_2.0.8_ubuntu-jammy_amd64.deb
 rm newrelic-infra_linux_1.48.4_amd64.tar.gz
 rm -rf newrelic-infra/
 
